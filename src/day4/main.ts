ada3a8330ec0a61fbd37e3f70a3fe7eb8e15d534
Bram Janssens <s.a.janssens@gmail.com>
2024-12-04 11:50:58 +0100

    day4.2

--- src/day4/main.ts
+++ src/day4/main.ts
@@ -25,6 +25,26 @@ result += countHorizontalBothWays(rotatedLines);
 // Result:
 console.log(result);
 
+// Part 2
+let result2 = 0
+const charMatrix2 = toCharMatrix(lines);
+for (let x = 0; x < charMatrix2[0].length - 1; x++) {
+    for (let y = 0; y < charMatrix2.length - 1; y++) {
+        let field3x3 = extractSubmatrix(charMatrix2, x, y)
+
+        let rotatedLines = toLines(rotate45Degrees(field3x3));
+        let hit1 = (rotatedLines[2].match(/MAS/)?.length ?? 0) == 1 || (rotatedLines[2].match(/SAM/)?.length ?? 0) == 1;
+
+        let rotatedMinusLines = toLines(rotateMinus45Degrees(field3x3));
+        let hit2 = (rotatedMinusLines[2].match(/MAS/)?.length ?? 0) == 1 || (rotatedMinusLines[2].match(/SAM/)?.length ?? 0) == 1;
+
+        if (hit1 && hit2)
+            result2++
+    }
+}
+console.log(result2);
+
+
 // Utils:
 function countHorizontalBothWays(lines: string[]) {
     let count = 0
@@ -96,3 +116,13 @@ function rotateMinus45Degrees(matrix: string[][]): string[][] {
     return diagonals;
 }
 
+function extractSubmatrix(matrix: string[][], startRow: number, startCol: number, size: number = 3): string[][] {
+    const submatrix: string[][] = [];
+
+    for (let i = 0; i < size; i++) {
+        const row = matrix[startRow + i].slice(startCol, startCol + size);
+        submatrix.push(row);
+    }
+
+    return submatrix;
+}
