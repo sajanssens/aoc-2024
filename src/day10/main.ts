986f13a2c17d62298fa3737753c30998b1afbee6
Bram Janssens <s.a.janssens@gmail.com>
2024-12-12 16:10:32 +0100

    day10.2

--- src/day10/main.ts
+++ src/day10/main.ts
@@ -6,19 +6,12 @@ const filePath = 'data.txt';
 const fileContent = readFileSync(filePath, 'utf8');
 let lines = fileContent.split("\r\n").filter(line => line !== "");
 
-interface Tile {
-    row: number
-    col: number
-    height: string
-    reachablePeaks: Tile[]
-}
 
-
-// Part 1
 (function () {
     let result = 0;
     let map = toCharMatrix(lines);
     let tiles: Tile[][] = []
+
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
             let height = map[i][j];
@@ -31,7 +24,7 @@ interface Tile {
         for (let j = 0; j < tiles[i].length; j++) {
             let tile = tiles[i][j];
             if (tile.height === "9") {
-                descent(tiles, tile, tile)
+                descentStart(tiles, tile)
             }
         }
     }
@@ -44,12 +37,13 @@ interface Tile {
     console.log(`Result: ${result} took ${Date.now() - start}ms`);
 })();
 
-// Part 2
-(function () {
-    let result = 0;
 
-    console.log(`Result: ${result} took ${Date.now() - start}ms`);
-})();
+interface Tile {
+    row: number
+    col: number
+    height: string
+    reachablePeaks: Tile[]
+}
 
 function newTile(i: number, j: number, height: string, reachablePeaks: Tile[] = []): Tile {
     return {row: i, col: j, height: height, reachablePeaks: reachablePeaks};
@@ -61,8 +55,8 @@ function descentStart(tiles: Tile[][], tile: Tile) {
 
 function descent(tiles: Tile[][], tile: Tile, startTile: Tile) {
     if (tile.height == "0") {
-        if (!tile.reachablePeaks.includes(startTile))
-            tile.reachablePeaks.push(startTile)
+        // if (!tile.reachablePeaks.includes(startTile)) // part 2, for part 1: activate this line
+        tile.reachablePeaks.push(startTile)
         return
     }
 
